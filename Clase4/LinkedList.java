0da0d15cf98cad731ee56a4d7e1742db6f5a6b68
catapecas <zoraya.giraldo@campusucc.edu.co>
2025-08-28 02:07:40 +0000

    index

--- Clase4/LinkedList.java
+++ Clase4/LinkedList.java
@@ -1,34 +1,44 @@
 public class LinkedList {
     private Node head;
-    
+
     public Node getHead() {
         return head;
     }
 
     public void setHead(Node head) {
         this.head = head;
-    } 
-     public void add (Object o){
-        if ((head==null)){
-            head=new Node(o);
+    }
 
-            
-        }else{
-            Node newHead=new Node(o);
+    public void add(Object o) {
+        if ((head == null)) {
+            head = new Node(o);
+
+        } else {
+            Node newHead = new Node(o);
             newHead.setNextnNode(head);
-            head=newHead;
+            head = newHead;
         }
     }
 
-    public String toString(){
-            Node iter = head;
-            String retuString="";
-            while (iter!=null){
-                retuString=retuString + " , " + iter.getData();
-                 iter=iter.getNextnNode();
-    
+    public String toString() {
+        Node iter = head;
+        String retuString = "";
+        while (iter != null) {
+            retuString = retuString + " , " + iter.getData();
+            iter = iter.getNextnNode();
+
         }
-     return retuString;   
-    
+        return retuString;
+    }
+
+    public int size() {
+        Node iter = head;
+        int size = 0;
+        while (iter != null) {
+            size++;
+            iter = iter.getNextnNode();
+        }
+        return size;
+
     }
 }
